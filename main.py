cbd38629f12d58454a10838ea89428add278d6a1
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2024-08-20 01:21:15 -0300

    Refactoring existent code

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import numpy as np
 import speech_recognition as sr
 import whisper
 import torch
-
 from datetime import datetime, timedelta
 from queue import Queue
 from time import sleep
@@ -14,41 +13,46 @@ import cv2
 import mediapipe as mp
 from utils.string_utils import remove_special_chars_and_accents
 
+# Configurações gerais
+SPECIAL_WORD = 'transição'
+SPECIAL_GESTURE = 'rock'  # peace, thumb_up, rock
+FONT_SCALE = 0.8
+RECORD_TIMEOUT = 2
+PHRASE_TIMEOUT = 3
+TEXT_COLOR = (0, 255, 255)  # Amarelo em BGR
+BACKGROUND_COLOR = (0, 0, 0)  # Preto em BGR
+FONT_THICKNESS = 2
 
-special_word = 'transição'
-special_gesture = 'peace' ## peace, thumb_up, rock
-font_scale = 0.8
-
+# Variáveis globais
 phrase_time = None
 data_queue = Queue()
-recorder = sr.Recognizer()
-recorder.energy_threshold = 1000
-recorder.dynamic_energy_threshold = False
-source = sr.Microphone(sample_rate=16000)
-
-# Load / Download model
-model = "base"
-audio_model = whisper.load_model(model)
-
-record_timeout = 2
-phrase_timeout = 3
 transcription = ['']
 special_word_found_description = None
 special_gesture_found_description = None
-
-with source:
-    recorder.adjust_for_ambient_noise(source)
-
-
-# Initialize Mediapipe solutions
 mp_hands = mp.solutions.hands
-hands = mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5, min_tracking_confidence=0.5)
-mp_pose = mp.solutions.pose
-pose = mp_pose.Pose(static_image_mode=False, min_detection_confidence=0.5, min_tracking_confidence=0.5)
-mp_drawing = mp.solutions.drawing_utils
-cap = cv2.VideoCapture(0)
-prev_hand_landmarks = None
 
+# Inicializa o reconhecimento de fala
+def init_speech_recognition():
+    recorder = sr.Recognizer()
+    recorder.energy_threshold = 1000
+    recorder.dynamic_energy_threshold = False
+    source = sr.Microphone(sample_rate=16000)
+    with source:
+        recorder.adjust_for_ambient_noise(source)
+    return recorder, source
+
+# Carrega o modelo Whisper
+def load_audio_model(model_name="base"):
+    return whisper.load_model(model_name)
+
+# Inicializa os módulos do Mediapipe
+def init_mediapipe():
+    mp_hands = mp.solutions.hands
+    hands = mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.8, min_tracking_confidence=0.9)
+    mp_drawing = mp.solutions.drawing_utils
+    return hands, mp_drawing
+
+# Verifica se o gesto é "thumb up"
 def is_thumb_up(hand_landmarks):
     thumb_tip = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
     thumb_ip = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_IP]
@@ -56,117 +60,124 @@ def is_thumb_up(hand_landmarks):
     index_finger_mcp = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_MCP]
     return thumb_tip.y < thumb_ip.y < thumb_mcp.y and thumb_mcp.y < index_finger_mcp.y
 
+# Verifica se o gesto é "peace"
 def is_peace_sign(hand_landmarks):
     return (hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y < hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_DIP].y and
             hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y < hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_DIP].y and
             hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_TIP].y > hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_DIP].y and
             hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_TIP].y > hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_DIP].y)
 
+# Verifica se o gesto é "rock"
 def is_rock_sign(hand_landmarks):
     return (hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y < hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_DIP].y and
             hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_TIP].y < hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_DIP].y and
             hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y > hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_DIP].y and
             hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_TIP].y > hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_DIP].y)
 
-def start_microphone(phrase_time, data_queue, audio_model, phrase_timeout, transcription, special_word):
-    global special_word_found_description
+# Desenha texto com fundo no frame
+def draw_text_with_background(frame, text, position, font_scale, color, thickness, background_color):
+    # Obtem o tamanho do texto
+    (text_width, text_height), baseline = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, font_scale, thickness)
+    x, y = position
+
+    # Desenha o fundo
+    cv2.rectangle(frame, (x, y - text_height - baseline), (x + text_width, y + baseline), background_color, cv2.FILLED)
+
+    # Desenha o texto
+    cv2.putText(frame, text, position, cv2.FONT_HERSHEY_SIMPLEX, font_scale, color, thickness, cv2.LINE_AA)
+
+# Função de callback para gravação de áudio
+def record_callback(_, audio: sr.AudioData) -> None:
+    data_queue.put(audio.get_raw_data())
+
+# Processa o áudio e atualiza as transcrições
+def start_microphone(audio_model):
+    global phrase_time, transcription, special_word_found_description
     while True:
         try:
             now = datetime.now()
-        # Pull raw recorded audio from the queue.
             if not data_queue.empty():
                 phrase_complete = False
-            # If enough time has passed between recordings, consider the phrase complete.
-            # Clear the current working audio buffer to start over with the new data.
-                if phrase_time and now - phrase_time > timedelta(seconds=phrase_timeout):
+                if phrase_time and now - phrase_time > timedelta(seconds=PHRASE_TIMEOUT):
                     phrase_complete = True
-            # This is the last time we received new audio data from the queue.
                 phrase_time = now
-            
-            # Combine audio data from queue
+                
                 audio_data = b''.join(data_queue.queue)
                 data_queue.queue.clear()
-            
-            # Convert in-ram buffer to something the model can use directly without needing a temp file.
-            # Convert data from 16 bit wide integers to floating point with a width of 32 bits.
-            # Clamp the audio stream frequency to a PCM wavelength compatible default of 32768hz max.
-                audio_np = np.frombuffer(audio_data, dtype=np.int16).astype(np.float32) / 32768.0
 
-            # Read the transcription.
+                audio_np = np.frombuffer(audio_data, dtype=np.int16).astype(np.float32) / 32768.0
                 result = audio_model.transcribe(audio_np, fp16=torch.cuda.is_available())
                 text = result['text'].strip()
 
-            # If we detected a pause between recordings, add a new item to our transcription.
-            # Otherwise edit the existing one.
                 if phrase_complete:
                     transcription.append(text)
                 else:
                     transcription[-1] = text
 
-            # Clear the console to reprint the updated transcription.
-                os.system('cls' if os.name=='nt' else 'clear')
+                os.system('cls' if os.name == 'nt' else 'clear')
                 for line in transcription:
                     print(line)
-                    if remove_special_chars_and_accents(special_word) in remove_special_chars_and_accents(line):
+                    if remove_special_chars_and_accents(SPECIAL_WORD) in remove_special_chars_and_accents(line):
                         transcription = []
-                        special_word_found_description = f"Corte detectado pela palavra {remove_special_chars_and_accents(special_word)}"
+                        special_word_found_description = f"Corte detectado pela palavra {remove_special_chars_and_accents(SPECIAL_WORD)}"
                         break
             else:
-            # Infinite loops are bad for processors, must sleep.
                 sleep(0.25)
         except KeyboardInterrupt:
             break
 
-
-def start_cam():
+# Processa o vídeo e detecta gestos
+def start_cam(hands, mp_drawing):
     global special_gesture_found_description
+    cap = cv2.VideoCapture(0)
+
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             break
 
-    # Convert the image to RGB
         rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        cv2.putText(frame, special_word_found_description, (100, 50), cv2.FONT_HERSHEY_SIMPLEX, font_scale, (0, 255, 255), 2, cv2.LINE_AA)
-        cv2.putText(frame, special_gesture_found_description, (100, 200), cv2.FONT_HERSHEY_SIMPLEX, font_scale, (0, 255, 255), 2, cv2.LINE_AA)
-    # Process hand landmarks
+        if special_word_found_description:
+            draw_text_with_background(frame, special_word_found_description or '', (100, 50), FONT_SCALE, TEXT_COLOR, FONT_THICKNESS, BACKGROUND_COLOR)
+        if special_gesture_found_description:
+            draw_text_with_background(frame, special_gesture_found_description or '', (100, 200), FONT_SCALE, TEXT_COLOR, FONT_THICKNESS, BACKGROUND_COLOR)
+
         hand_results = hands.process(rgb_frame)
         if hand_results.multi_hand_landmarks:
             for hand_landmarks in hand_results.multi_hand_landmarks:
-                if is_peace_sign(hand_landmarks) and special_gesture == 'peace':
+                if is_peace_sign(hand_landmarks) and SPECIAL_GESTURE == 'peace':
                     special_gesture_found_description = 'Corte detectado pelo gesto de paz'
-                elif is_thumb_up(hand_landmarks) and special_gesture == 'thumb_up':
+                elif is_thumb_up(hand_landmarks) and SPECIAL_GESTURE == 'thumb_up':
                     special_gesture_found_description = 'Corte detectado pelo gesto de joinha'
-                elif is_rock_sign(hand_landmarks) and special_gesture == 'rock':
+                elif is_rock_sign(hand_landmarks) and SPECIAL_GESTURE == 'rock':
                     special_gesture_found_description = 'Corte detectado pelo gesto de rock'
 
-
-    # Draw landmarks on the image
         if hand_results.multi_hand_landmarks:
             for hand_landmarks in hand_results.multi_hand_landmarks:
                 mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
         cv2.imshow('Recognition live', frame)
-
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+    cap.release()
+    cv2.destroyAllWindows()
 
-def record_callback(_, audio:sr.AudioData) -> None:
-    """
-    Threaded callback function to receive audio data when recordings finish.
-    audio: An AudioData containing the recorded bytes.
-    """
-    # Grab the raw bytes and push it into the thread safe queue.
-    data = audio.get_raw_data()
-    data_queue.put(data)
+# Inicializa os componentes e threads
+def main():
+    global phrase_time, transcription, special_word_found_description, special_gesture_found_description
 
-# Create a background thread that will pass us raw audio bytes.
-# We could do this manually but SpeechRecognizer provides a nice helper.
-recorder.listen_in_background(source, record_callback, phrase_time_limit=record_timeout)
+    recorder, source = init_speech_recognition()
+    audio_model = load_audio_model()
+    hands, mp_drawing = init_mediapipe()
 
-# Cue the user that we're ready to go.
-print("Model loaded.\n")
-cam_thread = Thread(target=start_cam)
-cam_thread.start()
+    recorder.listen_in_background(source, record_callback, phrase_time_limit=RECORD_TIMEOUT)
 
-start_microphone(phrase_time, data_queue, audio_model, phrase_timeout, transcription, special_word)
+    print("Model loaded.\n")
+
+    cam_thread = Thread(target=start_cam, args=(hands, mp_drawing))
+    cam_thread.start()
+
+    start_microphone(audio_model)
+
+if __name__ == '__main__':
+    main()
